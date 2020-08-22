bc152311ea957973dcd448442d8414747170b485
Eduard <eduardstancu@yahoo.com>
2020-08-22 23:41:03 +0300

    home page

--- src/Homepage.component.jsx
+++ src/Homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
@@ -9,18 +10,26 @@ const HomePage = () => {
             <h1 className='title'>HATS</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
+        </div>
+        <div className='menu-item'>
           <div className='content'>
             <h1 className='title'>JACKETS</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
+        </div>
+        <div className='menu-item'>
           <div className='content'>
             <h1 className='title'>SNEAKERS</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
+        </div>
+        <div className='menu-item'>
           <div className='content'>
             <h1 className='title'>WOMEN</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
+        </div>
+        <div className='menu-item'>
           <div className='content'>
             <h1 className='title'>MEN</h1>
             <span className='subtitle'>SHOP NOW</span>

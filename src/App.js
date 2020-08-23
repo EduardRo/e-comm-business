f6b72ea8e06afa5e2d3e5597718af4ade2ac4015
Eduard <eduardstancu@yahoo.com>
2020-08-23 16:02:44 +0300

    create navigation from menu-items using withRouter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import React from 'react';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Detail</h1>
+      <p>{props.match.params.topicId}</p>
+      <p>{props.match.params.param2}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hatpage' component={HatsPage} />
+        <Route
+          exact
+          path='/topics/:topicId?/:param2?'
+          component={TopicDetail}
+        />
+      </Switch>
     </div>
   );
 }
